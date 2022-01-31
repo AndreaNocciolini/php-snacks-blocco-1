b73f0aacb5f5b50b9fbaef0f85779cae5ddb5968
AndreaNocciolini <andreanocciolini93@gmail.com>
2022-01-31 15:00:54 +0100

    add snack 2

--- snack3.php
+++ snack3.php
@@ -1,11 +1,9 @@
-<!-- ## Snack 2
-Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
-## Snack 3
+<!-- ## Snack 3
 Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
-Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z
-## Snack 4
-Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
-## Snack 5
+Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z -->
+<!-- ## Snack 4
+Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+<!-- ## Snack 5
 Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. -->
 
 <!DOCTYPE html>
@@ -17,6 +15,8 @@ Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il pa
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>
+        <?php  ?>
+    </h1>
 </body>
 </html>
