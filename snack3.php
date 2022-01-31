55df62358b7a65d8f8235eecc2fc82476e3a7b19
AndreaNocciolini <andreanocciolini93@gmail.com>
2022-01-31 15:49:54 +0100

    snack 3?

--- snack3.php
+++ snack3.php
@@ -1,10 +1,53 @@
 <!-- ## Snack 3
 Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
 Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z -->
-<!-- ## Snack 4
-Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
-<!-- ## Snack 5
-Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. -->
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Lorem Ipsum'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Davide Mastrandea',
+            'text' => 'Stampare ogni dato'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Giuseppe Cozzaro',
+            'text' => 'Invisible no transfer'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Andrea Andreini',
+            'text' => 'Non esattamente'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Maria Giudici',
+            'text' => 'Quante verità'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Element Index',
+            'text' => '100% riciclabile'
+        ]
+    ],
+];
+
+// foreach ($posts as $post) {
+//     var_dump($post);
+// }
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -15,8 +58,8 @@ Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il pa
     <title>Document</title>
 </head>
 <body>
-    <h1>
-        <?php  ?>
-    </h1>
+    <p>
+        <?php print_r($posts) ?>
+    <p>
 </body>
 </html>
